7c8c7a9c4589ccd1efe829ff64f3c9da837f4074
arno <e7532161@gmail.com>
2024-05-10 01:29:40 -0700

    leetcode 207

---
+++ Graph/course_schedule.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        pre_map = {i: [] for i in range(numCourses)}
+        for course, pre_course in prerequisites:
+            pre_map[course].append(pre_course)
+
+        visited = set()
+
+        def dfs(course):
+            if pre_map[course] == None:
+                return True
+            if course in visited:
+                return False
+            visited.add(course)
+            for pre_course in pre_map[course]:
+                if dfs(pre_course) == False:
+                    return False
+            visited.remove(course)
+            pre_map[course] = []
+            return True
+
+        for course in range(numCourses):
+            if dfs(course) == False: return False
+        return True
+            
